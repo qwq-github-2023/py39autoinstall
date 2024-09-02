5195e1e9fde9435d6e8e970d90caa154b9f8059e
qwq <95283645+qwq-github-2023@users.noreply.github.com>
2024-09-02 12:16:36 +0800

    Update openssl_update.sh

--- openssl_update.sh
+++ openssl_update.sh
@@ -5,9 +5,10 @@ wget https://www.openssl.org/source/openssl-1.1.1w.tar.gz
 tar -zxf openssl-1.1.1w.tar.gz
 cd openssl-1.1.1w
 mv /usr/bin/openssl /usr/bin/openssl_old
+mv /usr/local/openssl /usr/local/openssl_old
 ./config
 make -j4
-make install
+make install -prefix=/usr/local/openssl
 sudo ln -s /usr/local/lib64/libssl.so.1.1 /usr/lib64/
 sudo ln -s /usr/local/lib64/libcrypto.so.1.1 /usr/lib64/
 ln -s /usr/local/bin/openssl /usr/bin/openssl
